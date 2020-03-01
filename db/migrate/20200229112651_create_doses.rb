2e9aa96934bf8e498462765d4795baea8ccc058e
Vanda Adams <vandamadams@gmail.com>
2020-03-01 11:28:11 +0000

    created database

--- db/migrate/20200229112651_create_doses.rb
+++ db/migrate/20200229112651_create_doses.rb
@@ -1,6 +1,9 @@
 class CreateDoses < ActiveRecord::Migration[6.0]
   def change
     create_table :doses do |t|
+      t.string :description
+      t.references :cocktail, foreign_key: true
+      t.references :ingredient, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_115217) do
+ActiveRecord::Schema.define(version: 2020_02_29_112651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,8 @@ ActiveRecord::Schema.define(version: 2020_02_20_115217) do
 
   create_table "doses", force: :cascade do |t|
     t.string "description"
-    t.bigint "cocktail_id", null: false
-    t.bigint "ingredient_id", null: false
+    t.bigint "cocktail_id"
+    t.bigint "ingredient_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
