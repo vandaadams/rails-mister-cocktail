84bd1c171d24e9ba6038278baa306082078e1c0e
Vanda Adams <vandamadams@gmail.com>
2020-03-08 19:47:06 +0000

    added JSON parsing to DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'JSON'
+require 'open-uri'
+
 puts "Clearing database"
+Dose.destroy_all
 Ingredient.destroy_all
 
 puts "Creating ingredients"
-Ingredient.create!(name: "lemon")
-Ingredient.create!(name: "crushed ice")
-Ingredient.create!(name: "mint leaves")
-Ingredient.create!(name: "vodka")
-Ingredient.create!(name: "white rum")
-Ingredient.create!(name: "sugar syrup")
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+buffer = open(url).read
+result = JSON.parse(buffer)
+
+
+result.each do |ingredient|
+  ingredient[1].each do |i|
+    ingredients = Ingredient.create!(name: "#{i.values.join('')}")
+    p ingredients
+  end
+  # Ingredient.create!(name: "#{ingredient['strIngredient1']}")
+end
+
+# Ingredient.create!(name: "lemon")
+# Ingredient.create!(name: "crushed ice")
+# Ingredient.create!(name: "mint leaves")
+# Ingredient.create!(name: "vodka")
+# Ingredient.create!(name: "white rum")
+# Ingredient.create!(name: "sugar syrup")
