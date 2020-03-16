a0eb09faa5d27f12797a4a1045ce7cead96bc6f8
Vanda Adams <vandamadams@gmail.com>
2020-03-16 21:36:25 +0000

    added json exception to forgery protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception [:json_request]
+
+  # protect_from_forgery unless: -> { request.format.json? }
 end
